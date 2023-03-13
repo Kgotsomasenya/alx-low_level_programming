30732d10da7dc64febda56995a653815d83399e3
Kgotso <kgotsomasenya.km@gmail.com>
2023-03-13 04:48:24 -0700

    task 1

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - Entry point
+ * _puts - prints a string.
+ * @str: string to be printed.
  *
  * Return: Always 0 (Success)
  */
-void _puts(char *str)
+void _puts(char *str);
 {
-	printf("%s", str);
+char str[] = "At the end of the day, my goal was to be the best hacker";
 
+_puts(str); "At the end of the day, my goal was to be the best hacker"
+return (0);
 }
